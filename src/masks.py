96f1ef2ff765c4d6d6546ca5a6428e1b31212138
ellGoltso <goltsovi3@gmail.com>
2025-06-10 18:42:26 +0300

    create logs for utils and masks

--- src/masks.py
+++ src/masks.py
@@ -1,18 +1,34 @@
+import logging
+
+masks_logger = logging.getLogger("masks")
+file_handler = logging.FileHandler("logs/masks.log", "w", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+file_handler.setFormatter(file_formatter)
+masks_logger.addHandler(file_handler)
+masks_logger.setLevel(logging.DEBUG)
+
+
 def get_mask_card_number(card_number: int) -> str:
     """Функция принимает на вход номер карты и возвращает ее маску."""
 
     card_number_list: list = []
     masked_card_number: str = ""
 
-    while card_number > 0:
-        card_number_list.append(card_number % 10)
-        card_number //= 10
+    try:
+        while card_number > 0:
+            card_number_list.append(card_number % 10)
+            card_number //= 10
+    except TypeError:
+        masks_logger.error("Неверный формат номера карты")
+        return ""
 
     card_number_list.reverse()
 
     if len(card_number_list) != 16:
+        masks_logger.error("Неверное число символов в номере карты")
         return ""
 
+    masks_logger.info("Выполняется маскировка номера карты")
     for i, v in enumerate(card_number_list):
         if 6 <= i <= 11:
             masked_card_number += "*"
@@ -25,6 +41,15 @@ def get_mask_card_number(card_number: int) -> str:
 def get_mask_account(account_number: int) -> str:
     """Функция принимает на вход номер счета и возвращает его маску."""
 
-    if len(str(account_number)) != 20:
+    if not isinstance(account_number, int):
+        masks_logger.error("Неверный формат номера счета")
         return ""
+
+    if len(str(account_number)) != 20:
+        masks_logger.error("Неверное число символов в номере счета")
+        return ""
+    masks_logger.info("Выполняется маскировка номера счета")
     return "**" + str(account_number)[-4:]
+
+
+get_mask_account(12345678912345671234)
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,12 @@
 import json
+import logging
+
+utils_logger = logging.getLogger("utils")
+file_handler = logging.FileHandler("logs/utils.log", "w", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+file_handler.setFormatter(file_formatter)
+utils_logger.addHandler(file_handler)
+utils_logger.setLevel(logging.DEBUG)
 
 
 def get_data(path: str) -> list:
@@ -6,13 +14,18 @@ def get_data(path: str) -> list:
     и возвращает список словарей с данными"""
 
     try:
+        utils_logger.info(f"Поиск списка финансовых транзакций в файле: {path}")
         with open(path, encoding="utf-8") as f:
             data = json.load(f)
     except json.JSONDecodeError:
+        utils_logger.error("Неверный формат файла")
         return []
     except FileNotFoundError:
+        utils_logger.error("Файл не найден")
         return []
 
     if isinstance(data, list):
+        utils_logger.info("Получен список транзакций")
         return data
+    utils_logger.error("В файле содержится не список")
     return []
